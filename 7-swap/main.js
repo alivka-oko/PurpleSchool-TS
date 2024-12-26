0ae61e1ba7965fd5ce6f4a464dfcb22c14025af3
alivka-oko <alina.okotnikova2002@gmail.com>
2024-12-26 12:33:07 +0300

    fix

--- 7-swap/main.js
+++ 7-swap/main.js
@@ -2,30 +2,12 @@
 function swapKeysAndValues(obj) {
     const swapped = {};
     for (const key in obj) {
-        const value = valueToString(obj[key]);
+        const value = obj[key];
         if (value) {
             swapped[value] = key;
         }
     }
     return swapped;
-}
-function valueToString(data) {
-    switch (typeof data) {
-        case "string":
-            return data;
-        case "number":
-        case "symbol":
-        case "bigint":
-        case "boolean":
-        case "function":
-            return data.toString();
-        case "object":
-            if (data === null)
-                return "null";
-            return JSON.stringify(data);
-        default:
-            return undefined;
-    }
 }
 const original = {
     a: "1",
--- 7-swap/main.ts
+++ 7-swap/main.ts
@@ -1,34 +1,16 @@
-function swapKeysAndValues<T extends Record<string, unknown>>(
+function swapKeysAndValues<T extends Record<string, any>>(
   obj: T
 ): Record<string, keyof T> {
   const swapped: Record<string, keyof T> = {};
 
   for (const key in obj) {
-    const value = valueToString(obj[key]);
+    const value = obj[key];
     if (value) {
       swapped[value] = key;
     }
   }
 
   return swapped;
-}
-
-function valueToString<T>(data: T): string | undefined {
-  switch (typeof data) {
-    case "string":
-      return data;
-    case "number":
-    case "symbol":
-    case "bigint":
-    case "boolean":
-    case "function":
-      return data.toString();
-    case "object":
-      if (data === null) return "null";
-      return JSON.stringify(data);
-    default:
-      return undefined;
-  }
 }
 
 const original = {
