32de7a587d09e7f3688a345aad4a4a2e27810bed
alivka-oko <alina.okotnikova2002@gmail.com>
2025-01-14 07:39:25 +0300

    edit 8-pick

--- 8-pick/main.ts
+++ 8-pick/main.ts
@@ -4,32 +4,26 @@ const user = {
   skills: ["typescript", "js"],
 };
 
-type Normalized<Type> = {
-  [Key in keyof Type]: Type[Key];
-};
-
-type key = string | number | symbol;
-
-function pickObjectKeys<T extends Record<key, any>>(
+function pickObjectKeys<T extends Record<string, any>>(
   user: T,
   argsArray: (keyof T)[] | keyof T
-): Normalized<T> {
+): Record<string, any> {
   if (Array.isArray(argsArray)) {
     return argsArray.reduce((acc, key) => {
       if (key in user) {
         acc[key] = user[key];
       }
       return acc;
-    }, {} as Normalized<T>);
+    }, {} as T);
   } else {
     return {
       [argsArray]: user[argsArray],
-    } as Normalized<T>;
+    };
   }
 }
 
-const res = pickObjectKeys<typeof user>(user, ["age", "skills"]);
-const res2 = pickObjectKeys<typeof user>(user, "name");
+const res = pickObjectKeys(user, ["age", "skills"]);
+const res2 = pickObjectKeys(user, "name");
 
 console.log(res);
 console.log(res2);
